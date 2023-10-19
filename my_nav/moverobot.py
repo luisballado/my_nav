886913e1235cec0e53914eabbc51a00463fed3e1
Luis Ballado <luis@madlab.mx>
2023-10-19 13:26:00 -0600

    error en valores de theta

--- my_nav/moverobot.py
+++ my_nav/moverobot.py
@@ -1,220 +1,220 @@
 import math
 import time
-
-from my_nav.euler_from_quaternion import *
-
-from geometry_msgs.msg import Twist
-from sensor_msgs.msg import LaserScan
-from nav_msgs.msg import Odometry 
+from math import pow, atan2, sqrt, asin, cos, sin
+from math import radians, degrees
+from nav_msgs.msg import Odometry
 from rclpy.node import Node
 import rclpy
 import os
-
-from math import pow, atan2, sqrt
-from math import radians, degrees
-
+from odometria.euler_from_quaternion import *
+from geometry_msgs.msg import Twist, Pose, Quaternion
 from geometry_msgs.msg import Pose2D
-import time
+from rclpy.executors import MultiThreadedExecutor
+
 class MoveRobotNode(Node):
     
     def __init__(self):
+
         super().__init__('move_robot_node')
-        
+
+        self.get_logger().info("Node move_robot_node Started")
+
         self.cmd_vel_pub = self.create_publisher(
             Twist,
             '/cmd_vel',
-            10)
-
-        self.scan_subs = self.create_subscription(
-            LaserScan,
-            '/scan',
-            self.scan_callback,
-            10)
-
-        #suscribirse al topico de odometria
+            10
+        )
+        
         self.odom_subs = self.create_subscription(
             Odometry,
             '/odom',
             self.odom_callback,
-            10)
+            10
+        )
 
-        #iniciar moviendo del robot
-        #al crear el robot
-        self.linear_velocity = 0.2
+        self.linear_velocity = 0.0
         self.angular_velocity = 0.0
-        
+
+        self.k1 = 0.5
+        self.k2 = 1
+
         self.pose = Pose2D()
-               
-        #ejecutar move_robot cada 1seg
-        #self.timer = self.create_timer(1.0, self.move_robot)
-        self.timer = self.create_timer(1.0, self.odom_callback)
+
+        self.pose.x = 0.0
+        self.pose.y = 0.0
+        self.theta = 0.0
+
+        self.goal_pose = Pose2D()
+        self.goal_pose.x = 0.0
+        self.goal_pose.y = 0.0
+        
+        self.start_path = False
+        
+        #self.odom_subs
+        #ejecuta cada cierto tiempo
+        self.timer = self.create_timer(0.005, self.move2goal)
+                
+    def do_something(self):
+        os.system('clear')
+        self.get_logger().info("Haciendo algo")
+        time.sleep(0.5)
         
     def stop_robot(self):
 
         print("DETENIENDO ROBOT")
-                
+
         msg = Twist()
-        msg.linear.x  = 0.0
+        msg.linear.x = 0.0
         msg.angular.z = 0.0
-            
+
         #publicar a velocidades
         self.cmd_vel_pub.publish(msg)
 
         print("ROBOT DETENIDO")
+
+            
+    #valor de a
+    def euclidean_distance(self, goal_pose):
+        return sqrt(pow((goal_pose.x - self.pose.x),2)+pow((goal_pose.y-self.pose.y),2))
+
+    #alpha
+    def alpha(self, goal_pose):
+
+        alph = atan2(
+            (goal_pose.y - self.pose.y),
+            (goal_pose.x-self.pose.x)
+        ) - 2*self.theta
+        
+        return alph
+
+    def linear_vel(self,goal_pose):
+
+        v = (self.k1 *
+             self.euclidean_distance(goal_pose) *
+             cos(self.alpha(goal_pose)))
+
+        if v > 1:
+            v = 1.0
+
+        if v < -1:
+            v = -1.0
+            
+        return v
     
-    def odom_callback(self, msg):
-        #cuando el suscriptor recibe un mensaje se actualiza la posicion
+    #_w_
+    def angular_vel(self,goal_pose):
 
-        position = msg.pose.pose.position
+        w = self.k2 * self.alpha(goal_pose)
+        + self.k1 * sin(self.alpha(goal_pose)) * cos(self.alpha(goal_pose))
+
+        if w > 1:
+            w = 1.0
+
+        if w < -1:
+            w = -1.0
+        
+        return w
+
+    def odom_callback(self,msg):
+
+        self.pose.x = round(msg.pose.pose.position.x,4)
+        self.pose.y = round(msg.pose.pose.position.y,4)
+        
         orientation = msg.pose.pose.orientation
-
         euler = euler_from_quaternion(
             orientation.x,
             orientation.y,
             orientation.z,
-            orientation.w)
-            
+            orientation.w
+        )
         roll,pitch,yaw = euler
-            
-        self.pose.x = round(position.x,4)
-        self.pose.y = round(position.y,4)
-        self.pose.theta = yaw
-
-        x = self.pose.x
-        y = self.pose.y
-        theta = yaw
-        #x = msg.pose.pose.position.x
-        #y = msg.pose.pose.position.y
-        #z = msg.pose.pose.position.z
         
-        #quat_x = msg.pose.pose.orientation.x
-        #quat_y = msg.pose.pose.orientation.y
-        #quat_z = msg.pose.pose.orientation.z
-        #quat_w = msg.pose.pose.orientation.w
-                
+        self.theta = round(msg.pose.pose.orientation.z,4)
+        #self.theta = yaw
+        
+
+        """
         self.get_logger().info(
             "Posición: ({}, {})".format(
-                x, y)
+                self.pose.x, self.pose.y)
         )
         
         self.get_logger().info(
             "Theta: ({})".format(
-                theta)
-        )
-
-    def euclidean_distance(self, goal_pose):
-        #Distancia euclidiana entre la posicion actual y la meta
-        return sqrt(pow((goal_pose.x - self.pose.x),2)+pow((goal_pose.y - self.pose.y),2))
-
-    def steering_angle(self, goal_pose):
-        #Direccion del angulo
-        return atan2(goal_pose.y - self.pose.y, goal_pose.x - self.pose.x)
-
-    def linear_vel(self,goal_pose,constant=1.5):
-        return constant * self.euclidean_distance(goal_pose)
-
-    def angular_vel(self, goal_pose, constant=15):
-        goal_angle = self.steering_angle(goal_pose)
-        current_angle = math.fmod(self.pose.theta,2*math.pi)
-        return constant * (math.fmod((goal_angle - current_angle + math.pi),(2 * math.pi)) - math.pi)
-
+                self.theta)
+        )        
+        """
     def move2goal(self):
+        
         #mover hacia el objetivo
-        goal_pose = Pose2D()
+        goal_pose = self.goal_pose
+        
+        if not self.start_path:
+                        
+            #Datos
+            point_x,point_y = input("Ingrese las coordenadas x,y:: ").split(",")
+            
+            self.goal_pose.x = float(point_x)
+            self.goal_pose.y = float(point_y)
 
-        #Entrada de datos
-        point_x,point_y = input("Ingrese las coordenadas x,y:: ").split(",")
-        goal_pose.x = float(point_x)
-        goal_pose.y = float(point_y)
-
-        distance_tolerance = 0.15
+            self.start_path = True
 
         vel_msg = Twist()
-
-        while rclpy.ok() and self.euclidean_distance(goal_pose) >= distance_tolerance:
+        distance_tolerance = 0.15
+        
+        if self.euclidean_distance(goal_pose) >= distance_tolerance:
             
             velocidad_linear = self.linear_vel(goal_pose)
             velocidad_angular = self.angular_vel(goal_pose)
 
-            print("v_lineal: ",velocidad_linear)
-            print("v_angular: ",velocidad_angular)                 
-            print("pose actual x: ",self.pose.x)
-            print("pose actual y: ",self.pose.y)
-            print("pose objetivo x: ",goal_pose.x)
-            print("pose objetivo y: ",goal_pose.y)
-            print("angulo actual:",self.pose.theta)
-            print("angulo objetivo:",self.steering_angle(goal_pose))
-            print("distancia euclidiana:",self.euclidean_distance(goal_pose))
-            print("--------------------------------------------------------")
-                        
-	    # Linear velocity in the x-axis.
-            vel_msg.linear.x = 42.42640687119285 #velocidad_linear
-            vel_msg.linear.y = 0.0
-            vel_msg.linear.z = 0.0
+            os.system('clear')
             
-	    # Angular velocity in the z-axis.
+            self.get_logger().info(
+                "Posición: ({}, {})".format(
+                    self.pose.x, self.pose.y)
+            )
+
+            self.get_logger().info(
+                "Theta: ({})".format(
+                    self.theta)
+            )
+            
+            self.get_logger().info(
+                "Objetivo: ({}, {})".format(
+                    goal_pose.x, goal_pose.y)
+            )
+
+            self.get_logger().info("Vlineal: ({})".format(velocidad_linear))
+            self.get_logger().info("VAngular: ({})".format(velocidad_angular))
+            self.get_logger().info(
+                "D Euclidiana: ({})".format(
+                    round(self.euclidean_distance(goal_pose),3))
+            )
+
+            self.get_logger().info("Alpha: ({})".format(self.alpha(goal_pose)))
+            
+            vel_msg.linear.x = velocidad_linear #* cos(self.theta)
+            vel_msg.linear.y = 0.0 #velocidad_linear * sin(self.theta)
+            vel_msg.linear.z = 0.0
+
             vel_msg.angular.x = 0.0
             vel_msg.angular.y = 0.0
-            vel_msg.angular.z = 11.780972450961723 # velocidad_angular
-            
-	    # Publishing our vel_msg
+            vel_msg.angular.z = velocidad_angular
+
             self.cmd_vel_pub.publish(vel_msg)
-
-            time.sleep(1)
             
-        # Stopping our robot after the movement is over.
-        vel_msg.linear.x = 0.0
-        vel_msg.angular.z = 0.0
-        
-        self.cmd_vel_pub.publish(vel_msg)
-        
-        answer = input("Quieres ingresar otro punto? y/n:  ")
-
-        if answer == "y":
-            self.move2goal()
         else:
-            rclpy.shutdown()
-            sys.exit()
 
-                    
-    def scan_callback(self, msg):
+            vel_msg.linear.x = 0.0
+            vel_msg.angular.z = 0.0
 
-        os.system('clear')
-        
-        ranges = msg.ranges
-        
-        obstacle_threshold = 1.0
-                
-        obstacle_right = any(r < obstacle_threshold for r in ranges[90:135])
-        obstacle_center = any(r < obstacle_threshold for r in ranges[135:225])
-        obstacle_left = any(r < obstacle_threshold for r in ranges[225:270])
-        
-        self.linear_velocity = 0.2
-        self.angular_velocity = 0.0
-        
-        if obstacle_left:
-            self.linear_velocity = 0.00
-            self.angular_velocity = -0.2#0.2
-            self.get_logger().info('Izq: %f - angl vel: %f' %(self.linear_velocity, self.angular_velocity))
+            self.cmd_vel_pub.publish(vel_msg)
             
-        elif obstacle_right:
-            self.linear_velocity = 0.00
-            self.angular_velocity = 0.2#-0.2
-            self.get_logger().info('Der: %f - angl vel: %f' %(self.linear_velocity, self.angular_velocity))
+            self.start_path = False
+            answer = input("Quieres ingresar otro punto? y/n:   ")
             
-        elif obstacle_center:
-            self.angular_velocity = 0.2
-            self.linear_velocity = 0.0
-            self.get_logger().info('Fren: %f - angl vel: %f' %(self.linear_velocity, self.angular_velocity))
-            
-    def move_robot(self):
-
-        msg = Twist()
-        
-        msg.linear.x  = self.linear_velocity
-        msg.angular.z = self.angular_velocity
-        
-        #publicar a velocidades
-        self.cmd_vel_pub.publish(msg)
-        self.get_logger().info('Moviendo el robot:: veloc linear: %f velc ang: %f' % (self.linear_velocity, self.angular_velocity))
-    
+            if answer == "y":
+                pass
+            else:
+                rclpy.shutdown()
+                exit(-1)
--- my_nav/my_python_node.py
+++ my_nav/my_python_node.py
@@ -1,22 +1,20 @@
 import rclpy
 from my_nav.moverobot import *
-import signal
 
 def main(args=None):
     
     rclpy.init(args=args)
 
     node = MoveRobotNode()
-    node.move2goal()
-    
+        
     try:
         rclpy.spin(node)
     except KeyboardInterrupt:
         pass
-    finally:
-        node.stop_robot()
-        node.destroy_node()
-        #rclpy.shutdown()
+    
+    node.stop_robot()
+    node.destroy_node()
+    rclpy.shutdown()
                 
 if __name__ == '__main__':
     main()
